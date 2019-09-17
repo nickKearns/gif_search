bd6bb23b18ff826acbd0e0e28e11ecd3cedbdc3f
nickKearns <nickkearns133@gmail.com>
2019-09-17 10:10:48 -0700

    [add] show trending gifs button

--- app.py
+++ app.py
@@ -30,34 +30,58 @@ def index():
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
 
-    # get the data from the api using the apikey, the limit and the search term that the user has supplied
 
-    r = requests.get("https://api.tenor.com/v1/search?", params=params)
 
-    if r.status_code == 200:
-        # TODO: Use the '.json()' function to get the JSON of the returned response
-        # object
+    show_trending = request.args.get('show_trending')
+
+    if show_trending:
+        r = requests.get("https://api.tenor.com/v1/trending?key=XFF92IL8UZZG")
+
         json_gifs = json.loads(r.content)
         gifs = json_gifs['results']
-
-        print(gifs)
         return render_template(
             'index.html',
-            gifs=gifs,
-            search_term = search_term
+            gifs = gifs
         )
 
-    # convert that data into json data
 
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
-    # which contains the GIFs as a list
 
-    # assign the "results" portion of the json data to a variable called gifs, which should be a list of objects
 
-    # TODO: Render the 'index.html' template, passing the list of gifs as a
-    # named parameter called 'gifs'
 
-    # Pass index.html the list of gifs and the search term that was used to get those gifs.
+    # get the data from the api using the apikey, the limit and the search term that the user has supplied
+    elif search_term:
+
+        r = requests.get("https://api.tenor.com/v1/search?", params=params)
+
+
+        if r.status_code == 200:
+            # TODO: Use the '.json()' function to get the JSON of the returned response
+            # object
+            json_gifs = json.loads(r.content)
+
+            # TODO: Using dictionary notation, get the 'results' field of the JSON,
+            # which contains the GIFs as a list
+            gifs = json_gifs['results']
+
+            print(gifs)
+
+            # TODO: Render the 'index.html' template, passing the list of gifs as a
+            # named parameter called 'gifs'
+
+            return render_template(
+                'index.html',
+                gifs=gifs,
+                search_term = search_term
+            )
+    else:
+        r = requests.get("https://api.tenor.com/v1/search?", params=params)
+        json_gifs = json.loads(r.content)
+        gifs = json_gifs['results']
+        return render_template(
+            'index.html',
+            gifs=gifs
+        )
+    
 
 
 if __name__ == '__main__':
